f06903b6c0e90fc73c4759b4b91856c85e08b1cc
Huski-commando <gyan.mu@gmail.com>
2024-02-14 18:44:42 +0530

    installed shadcn ui library

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,12 @@
+import { Button } from "@/components/ui/button";
+
 const Home = () => {
-  return <div className="underline">Home</div>;
+  return (
+    <div className="underline uppercase">
+      <h1>Home</h1>
+      <Button>Click</Button>
+    </div>
+  );
 };
 
 export default Home;
