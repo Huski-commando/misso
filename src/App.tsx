bfebebf11d088ac243512628abb579154a380e77
Huski-commando <gyan.mu@gmail.com>
2024-02-14 19:34:38 +0530

    created components

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import Loader from "./components/Loader";
 const Home = lazy(() => import("./pages/Home"));
 const Cart = lazy(() => import("./pages/Cart"));
 const Search = lazy(() => import("./pages/Search"));
+const Contact = lazy(() => import("./pages/Contact"));
+const OrderHistory = lazy(() => import("./pages/OrderHistory"));
 
 const App = () => {
   return (
@@ -14,6 +16,8 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/search" element={<Search />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/order-history" element={<OrderHistory />} />
         </Routes>
       </Suspense>
     </BrowserRouter>
