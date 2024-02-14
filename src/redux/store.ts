87966edbe4ce2792e3cc6b332c601fc3a39de585
Huski-commando <gyan.mu@gmail.com>
2024-02-15 01:28:10 +0530

    developed theme

---
+++ src/redux/store.ts
@@ -0,0 +1,30 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import themeReducer from "./theme/themeSlice";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+
+const rootReducer = combineReducers({
+  theme: themeReducer,
+});
+
+const persistConfig = {
+  key: "root",
+  storage,
+  version: 1,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ serializableCheck: false }),
+});
+
+export const persistor = persistStore(store);
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch;
