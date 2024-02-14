3951311b1b611ea9e7bf9738521fa0719b343a70
Huski-commando <gyan.mu@gmail.com>
2024-02-14 21:04:18 +0530

    setting up redux toolkit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,9 +4,14 @@ import { IoSunnyOutline } from "react-icons/io5";
 import MobileNav from "./MobileNav";
 
 const Header = () => {
+  const activeClasses = ({ isActive }: isActiveProps) =>
+    isActive
+      ? `text-transparent bg-clip-text bg-gradient-to-br from-emerald-600 from-10% via-sky-500 via-50% to-emerald-500 to-80% italic`
+      : ``;
+
   return (
     <header className="border-b">
-      <nav className="max-w-7xl mx-auto py-2 px-6 xl:px-0">
+      <nav className="max-w-7xl mx-auto py-3 px-6 xl:px-0">
         <div className="flex items-center justify-between">
           <Link to="/">
             <Logo />
@@ -23,11 +28,21 @@ const Header = () => {
               <IoSunnyOutline />
             </span>
             <span className="space-x-4 hidden lg:flex">
-              <NavLink to="/">Home</NavLink>
-              <NavLink to="/contact">Contact</NavLink>
-              <NavLink to="/products">Products</NavLink>
-              <NavLink to="/cart">Cart</NavLink>
-              <NavLink to="/login">Login</NavLink>
+              <NavLink to="/" className={activeClasses}>
+                Home
+              </NavLink>
+              <NavLink to="/contact" className={activeClasses}>
+                Contact
+              </NavLink>
+              <NavLink to="/products" className={activeClasses}>
+                Products
+              </NavLink>
+              <NavLink to="/cart" className={activeClasses}>
+                Cart
+              </NavLink>
+              <NavLink to="/login" className={activeClasses}>
+                Login
+              </NavLink>
             </span>
 
             <MobileNav />
---
+++ src/lib/types.ts
@@ -0,0 +1,3 @@
+interface isActiveProps {
+  isActive: boolean;
+}
